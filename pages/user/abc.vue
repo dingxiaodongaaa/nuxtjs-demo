e7bae4a9ee67bbb74cd826fb87e5acdef6fddf21
dingxd <dingxiaodongaaa@163.com>
2020-10-21 10:34:29 +0800

    路由的基本使用

---
+++ pages/user/abc.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>UserAbcPage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User-Abc-Page"
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>UserPage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User-Page"
+}
+</script>
+
+<style>
+
+</style>
