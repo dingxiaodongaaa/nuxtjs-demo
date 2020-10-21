bdadece4c4618fe2f52fab5099814e586cac4965
dingxd <dingxiaodongaaa@163.com>
2020-10-21 11:48:10 +0800

    布局组件

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>layouts/default.vue组件</h1>
+    <!-- 页面出口，类似于子路由出口 -->
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LayoutsDefault"
+}
+</script>
+
+<style>
+
+</style>
