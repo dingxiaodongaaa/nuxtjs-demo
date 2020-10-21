a4d24c0f40a79e05a1792452ab5fd9240fc9b536
dingxd <dingxiaodongaaa@163.com>
2020-10-21 11:13:56 +0800

    自定义路由配置

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  router: {
+    base: "/abc",
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: 'custom',
+        path: '*',
+        component: resolve(__dirname, 'pages/404.vue')
+      })
+    }
+  }
+}
