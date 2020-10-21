818b50dc962d8c16857da96e0b1181ddb339950b
dingxd <dingxiaodongaaa@163.com>
2020-10-21 10:48:11 +0800

    路由导航

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
     <h1>About</h1>
+    <!-- a 链接，刷新导航，走服务端渲染 -->
+    <h2>a 链接</h2>
+    <a href="/">首页</a>
+
+    <!-- router-link 导航链接组件 -->
+    <h2>router-link 链接</h2>
+    <router-link to="/">首页</router-link>
+
+    <!-- 编程式导航 -->
+    <h2>编程式导航</h2>
+    <button @click="onclick">首页</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "About"
+  name: "About",
+  methods: {
+    onclick () {
+      this.$router.push("/")
+    }
+  }
 }
 </script>
 
